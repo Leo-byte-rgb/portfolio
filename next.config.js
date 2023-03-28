289dead37c5ae718a04c14fc87a5a8dd3041c417
leo-byte-rgb <leonardoagostininho@gmail.com>
2023-03-27 21:35:20 -0300

    chore(distDir): remove dist dir config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "build",
   images: {
     domains: [
       "https://media.licdn.com/dms/image/D4D03AQFaKesdZIJSpw/profile-displayphoto-shrink_400_400/0/1679253693791?e=1684972800&v=beta&t=ybDth5gewC-L9HyJo7aavKG2IQnsOmGjsAv3MzLCIQI",
